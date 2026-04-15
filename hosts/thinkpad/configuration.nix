d0f73d0a2743f745db3fa9c75a43a887afc08bd0
bkjn2040 <bkjn415@gmail.com>
2026-04-15 20:28:18 +0900

    Change thinkpad timezone

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -24,7 +24,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Asia/Seoul";
+  time.timeZone = "Europe/Amsterdam";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
