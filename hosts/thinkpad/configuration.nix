1d5df6b57ce3a582adb22d91585282a1682953a3
bkjn2040 <bkjn415@gmail.com>
2026-04-15 20:32:25 +0900

    Enable tailscale

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -78,6 +78,8 @@
 
   programs.hyprland.enable = true;
 
+  services.tailscale.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
