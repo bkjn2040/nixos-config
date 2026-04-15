b22dfb3d9c1fab576e82abfe9c677a2430d0314e
bkjn2040 <bkjn415@gmail.com>
2026-04-15 13:39:08 +0200

    Enable noisetorch

--- hosts/workstation/configuration.nix
+++ hosts/workstation/configuration.nix
@@ -179,6 +179,8 @@
   
   programs.hyprland.enable = true;
 
+  programs.noisetorch.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
