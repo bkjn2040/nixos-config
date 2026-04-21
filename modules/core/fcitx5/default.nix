c90fdd2b014ca30a51363f284a498d6982db9a36
bkjn2040 <bkjn415@gmail.com>
2026-04-21 09:30:08 +0200

    Declarative fcitx configuration

--- modules/core/fcitx5/default.nix
+++ modules/core/fcitx5/default.nix
@@ -9,6 +9,18 @@
         fcitx5-hangul
         fcitx5-gtk
       ];
+      settings.inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "hangul";
+        };
+        "Groups/0/Items/0".Name = "keyboard-us";
+        "Groups/0/Items/1".Name = "keyboard-us-intl";
+        "Groups/0/Items/2".Name = "hangul";
+      };
+      ignoreUserConfig = true;
     };
   };
 }
